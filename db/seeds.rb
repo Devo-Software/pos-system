157cb9a40adc0782f2fbf2d3c8600b1c3c3f139e
noguerajulioces <noguerajulio24@gmail.com>
2025-04-20 15:39:10 -0300

    refactor: simplify seeds.rb by removing redundant comments and adding account_id
    The seeds.rb file was cleaned up by removing unnecessary comments and ensuring all records are associated with the correct account_id. This improves maintainability and consistency in the database seeding process.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-Account.create!(name: 'Ferreteria el Rey')
+# First create the account
+account = Account.create!(name: 'Ferreteria el Rey')
 
-User.create(name: 'Administrator', email: 'admin@admin.com', password: 123456, account_id: Account.first.id)
+# Then create the user with the account reference
+User.create(name: 'Administrator', email: 'admin@admin.com', password: 123456, account_id: account.id)
 
-# db/seeds.rb
+# Now create the unit with the account reference
+unit = Unit.find_or_create_by!(name: 'Pieza', account_id: account.id)
 
-# Asegúrate de que exista una unidad para los productos
-unit = Unit.find_or_create_by!(name: 'Pieza')
-
-# Add currency seeds
 currencies = [
   { name: 'Dólar', code: 'USD', symbol: '$', exchange_rate: 7350, flag_url: 'https://flagcdn.com/w20/us.png', display: true },
   { name: 'Peso Argentino', code: 'ARS', symbol: '$', exchange_rate: 85.5, flag_url: 'https://flagcdn.com/w20/ar.png', display: true },
-  { name: 'Real Brasileño', code: 'BRL', symbol: 'R$', exchange_rate: 1250, flag_url: 'https://flagcdn.com/w20/br.png', display: true },
-  { name: 'Guarani', code: 'PYG', symbol: '₲', exchange_rate: 1, flag_url: 'https://flagcdn.com/w20/py.png', display: true }
+  { name: 'Real Brasileño', code: 'BRL', symbol: 'R$', exchange_rate: 1250, flag_url: 'https://flagcdn.com/w20/br.png', display: true }
 ]
 
+# Add account_id to each currency
 currencies.each do |currency_data|
-  Currency.find_or_create_by!(code: currency_data[:code]) do |currency|
-    currency.update(currency_data)
+  Currency.find_or_create_by!(code: currency_data[:code], account_id: account.id) do |currency|
+    currency.update(currency_data.merge(account_id: account.id))
   end
 end
 
@@ -44,6 +34,7 @@ company_settings = [
   { var: 'company_economic_activity', value: 'VENTA DE PRODUCTOS ELECTRÓNICOS' }
 ]
 
+# Create settings without the account_id parameter
 company_settings.each do |setting|
   Setting.set(setting[:var], setting[:value])
 end
